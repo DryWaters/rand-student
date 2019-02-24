2cb9d16b4db046ea728b20ca57a083f1a455be25
Daniel Waters <danwater1@gmail.com>
2019-02-24 12:19:05 -0800

    Modifies controls and students to fit 40 on a page at 1024x768

--- src/components/Student/index.js
+++ src/components/Student/index.js
@@ -3,7 +3,7 @@ import styles from '../../styles/components/Student/Student.module.css';
 
 const Student = ({student, toggleStudent}) => (
     <div className={`${styles.student} ${student.isPicked ? styles.picked : styles.unpicked}`} onClick={()=>toggleStudent(student)}>
-      <p>{student.id}</p>
+      <p className={styles.studentId}>{student.id}</p>
     </div>
   )
 
